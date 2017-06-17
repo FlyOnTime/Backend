c7d58b8b9d7c21ad79d55e7f9b707f0d8d828f75
Reinhard <reinhard.schauer@gmail.com>
2017-06-18 01:58:02 +0200

    API tests

---
+++ routes/backend.ts
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var client = new (require('node-rest-client').Client);
+
+// 
+
+router.get("/travelInfo", function(req, res){
+    var resultSchema = {
+        id: "122016526840710660",
+        flightName: "CAI8002",
+        prefixIATA: "XC",
+        prefixICAO: "CAI",
+        flightNumber: "8002",
+        scheduleDate: "2017-06-17",
+        scheduleTime: "03:45:00",
+        destination: "AYT",
+        terminal: "3",
+        gate: "G05",
+        checkinStartTime: "2017-06-17T00:45:00.000+02:00",
+        checkinEndTime: "2017-06-17T03:00:00.000+02:00",
+        checkInPosition: ["30", "31"],
+        expedtedTimeBoarding: "2017-06-17T03:15:00.000+02:00",
+        expectedTimeGateOpen: "2017-06-17T01:00:00.000+02:00",
+        expectedTimeGateClosing: "2017-06-17T03:35:00.000+02:00",
+        luggageInformation: {
+            itemNumber: 2,
+            itemAllowance: "max. 23 kg, 1 bag",
+            specialLuggageNumber: 1,
+            specialLuggageType: "bicycle",
+            specialLuggageAllowance: "max. 25 kg, 1 bag"
+        },
+        estimatedWaitingTimeTotalSeconds: "1200",
+        estimatedIndoorWalkingDurationSeconds: "900",
+        estimatedCheckInWaitingTimeSeconds: "720",
+        estimatedSecurityCheckWaitingTimeSeconds: "1140",
+        estimatedSecurityCheckWaitingTimeAccuracy: "94%"    
+    };
+    res.send(resultSchema);
+});
+
+router.get("/flightInfo/:airlineCode/:flightNumber/:originFlightDate", function(req, res) {
+    var airlineCode = req.params.airlineCode;
+    var flightNumber = req.params.flightNumber;
+    var originFlightDate = req.params.originFlightDate;
+
+
+    // gather information for this flight
+    // 
+    getAMSFlightData(airlineCode, flightNumber, originFlightDate);
+
+    var args = {
+        headers: {}
+    };
+    args["headers"]["X-apiKey"] = "d017a45398ba4a8e14b7fe534fb9b54a";
+    client.get("https://api-dev.munich-airport.de/aci-flight-v1/flightDetails/" 
+        + airlineCode + "/" + flightNumber + "/" + originFlightDate, 
+        args, function(data, response){
+            console.log(data);        
+            res.send(data);
+    });
+});
+
+function getAMSFlightData(airlineCode, flightNumber, originFlightDate) {
+    // https://api-acc.schiphol.nl/public-flights/flights?app_id=a5ce65e3&app_key=34e2fc05aed1f047beeedb6c629972c1&airline=LH
+    var args = {
+        headers: {ResourceVersion: "v3"},
+        parameters: { app_id: "a5ce65e3", app_key: "34e2fc05aed1f047beeedb6c629972c1", airline: "KL"}
+    };
+    // args["headers"]["ResourceVersion: v3"] = "d017a45398ba4a8e14b7fe534fb9b54a";
+    client.get("https://api-acc.schiphol.nl/public-flights/flights", 
+        args, function(data, response){
+            console.log("getAMSFlightData");
+            console.log(data);        
+    });
+}
+
+module.exports = router;
--- server.ts
+++ server.ts
@@ -3,8 +3,27 @@
  */
 import express = require('express');
 import htmlparser = require("htmlparser2");
-import jsonld = require('jsonld');
+import jsonld = require('jsonld'); 
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 const app = express();
+
+var apitest = require("./routes/apitest");
+var backend = require("./routes/backend");
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/apitest", apitest);
+app.use("/backend", backend);
 
 let parser = new htmlparser.Parser({
     onopentag: function(name, attribs){
@@ -30,11 +49,14 @@ let parser = new htmlparser.Parser({
 parser.write('<html> <head> <script type="application/ld+json"> { "@context": "http://schema.org", "@type": "FlightReservation", "reservationNumber": "RXJ34P", "reservationStatus": "http://schema.org/Confirmed", "underName": { "@type": "Person", "name": "Eva Green" }, "reservationFor": { "@type": "Flight", "flightNumber": "110", "airline": { "@type": "Airline", "name": "United", "iataCode": "UA" }, "departureAirport": { "@type": "Airport", "name": "San Francisco Airport", "iataCode": "SFO" }, "departureTime": "2017-03-04T20:15:00-08:00", "arrivalAirport": { "@type": "Airport", "name": "John F. Kennedy International Airport", "iataCode": "JFK" }, "arrivalTime": "2017-03-05T06:30:00-05:00" }, "airplaneSeat": "9A", "airplaneSeatClass": { "@type": "AirplaneSeatClass", "name": "Business" }, "ticketNumber": "ABC1234", "ticketToken": "qrCode:AB34" } </script> </head> <body> <p> This a test for a Go-To action in Gmail. </p> </body> </html>');
 parser.end();
 
+// Google Maps Directions API Key
+// AIzaSyBIqZZoPNUY72VvnJQEvukFgBOXWShvobU
+
 //Basic route
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(3000 || process.env.PORT, "0.0.0.0", function () {
     console.log('Example app listening on port 3000!')
 });
