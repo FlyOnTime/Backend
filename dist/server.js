88b1b9e52108adb7d4f51e9025bc94e1d5305350
Reinhard <reinhard.schauer@gmail.com>
2017-06-18 11:00:00 +0200

    thanks heroku

--- dist/server.js
+++ dist/server.js
@@ -50,6 +50,6 @@ parser.end();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(3000 || process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
