2a1979d3c7f5a85aaa32d8972e98b2196097d70c
Jossi Wolf <jossiwolf@gmx.net>
2017-07-19 18:52:28 +0200

    Returning the right stuff :)

--- src/controllers/UserStatusController.ts
+++ src/controllers/UserStatusController.ts
@@ -1,4 +1,4 @@
-import {Get, HttpCode, JsonController} from "routing-controllers";
+import {Get, HttpCode, JsonController, OnUndefined} from "routing-controllers";
 
 //TODO: Migrate routes from @link{backend.ts} here
 
@@ -6,10 +6,10 @@ import {Get, HttpCode, JsonController} from "routing-controllers";
 export class UserStatusController {
 
     // This route should return 204;
-    // TODO: This route should just return nothing. @see{https://github.com/pleerock/routing-controllers/issues/224}
     @Get("/status")
     @HttpCode(204)
+    @OnUndefined(404)
     updateUserStatus() {
-        return {};
+        return null;
     }
 }
